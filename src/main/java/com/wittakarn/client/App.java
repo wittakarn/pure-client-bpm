a1ba88c28c748e854e07e0424d2ad18bcb28f66b
Wittakarn <wittakarn.expert@gmail.com>
2014-09-08 23:36:04 +0700

    Modify App.java in order to demonstrate control coupling.

--- src/main/java/com/wittakarn/client/App.java
+++ src/main/java/com/wittakarn/client/App.java
@@ -11,8 +11,11 @@ import com.wittakarn.pure.client.bonita.BonitaItem;
 import com.wittakarn.pure.client.bonita.BonitaWrapper;
 import com.wittakarn.pure.client.oracle.OracleItem;
 import com.wittakarn.pure.client.oracle.OracleWrapper;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import oracle.bpel.services.workflow.WorkflowException;
 import org.bonitasoft.engine.exception.BonitaException;
 
@@ -22,7 +25,34 @@ import org.bonitasoft.engine.exception.BonitaException;
  */
 public class App {
     public static void main(String args[]){
+    }
+    
+    public List<LeaveItem> searchTask(String vendor){
+        List<LeaveItem> leaveItems = new ArrayList<LeaveItem>();
         
+        if(vendor.equals("Oracle")){
+            try {
+                OracleItem oracleItem = new OracleItem();
+                oracleItem.setUserId("xxx");
+                oracleItem.setPassword("yyy");
+                searchOracleTask(oracleItem);
+            } catch (WorkflowException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }else if(vendor.equals("Bonita")){
+            BonitaItem bonitaItem = new BonitaItem();
+            bonitaItem.setUserId("xxx");
+            bonitaItem.setPassword("yyy");
+            try {
+                searchBonitaTask(bonitaItem);
+            } catch (BonitaException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        //map list of HashMap to listof leaveItem.
+        
+        return leaveItems;
     }
     
     private List<LeaveItem> searchBonitaTask(BonitaItem bonitaItem) throws BonitaException{
